a812de5b84aa61fff59b8ee739ed86eabadb3844
Hanzala <bigapplestorekmr@gmail.com>
2025-05-29 16:14:58 +0530

    ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ function App() {
   
 
   return (
-    <div className='flex justify-center bg-[#e5d9c7] items-center ' >
-      <div className='flex flex-col border-3 rounded-4xl border-bg-[#6e7163] px-40 space-y-5 bg-[#472828] text-[#e5d9c7] py-50'>
-        <h1 className='text-5xl font-mono mt-0'> Calculator </h1>
-        <input  type='number' className='w-full  border text-2xl '/>
+    <div className='flex min-h-screen  justify-center bg-[#e5d9c7] items-center ' >
+      <div className='flex  flex-col border-3 rounded-4xl border-bg-[#6e7163] px-50 space-y-5 bg-[#472828] text-[#e5d9c7] py-40'>
+        <h1 className='text-5xl font-mono mt-0 hover:font-extrabold'> Calculator </h1>
+        <input  type='number' className='w-full  hover:bg-black border text-2xl '/>
         <div className='flex items-center space-4 space-x-4'>
           <DisplayNumber value={1} ></DisplayNumber>
           <DisplayNumber value={2} ></DisplayNumber>
@@ -29,7 +29,7 @@ function App() {
 
 
         </div>
-        <div className='flex space-4 items-center space-x-4'>
+        <div className='flex space-4 space-x-4'>
           <DisplayNumber value={7} ></DisplayNumber>
           <DisplayNumber value={8} ></DisplayNumber>
           <DisplayNumber value={9} ></DisplayNumber>
--- src/components/Number.tsx
+++ src/components/Number.tsx
@@ -2,7 +2,7 @@
   
   export default function DisplayNumber({ value, onclick}: any) {
     return (
-      <button  onClick={onclick} className="px-6 py-3 border flex ">
+      <button  onClick={onclick} className="px-6 rounded-4xl text-lg font-semibold py-3 border-3 hover:bg-black flex ">
         {value}
       </button>
     );
